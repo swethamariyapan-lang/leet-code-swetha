60aaadab22c0083ba7451b3a3c0b0255929603af
swethamariyapan-lang <swethamariyapan@gmail.com>
2026-07-22 09:10:18 +0530

    Time: 1 ms (50.24%), Space: 19.4 MB (23.31%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        for folder in path.split("/"):
+            if folder == "" or folder == ".":
+                continue
+            elif folder == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(folder)
+
+        return "/" + "/".join(stack)
+        
